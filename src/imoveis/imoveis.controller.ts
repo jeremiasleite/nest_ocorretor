5442c81f5d0a761f46f2a20b027b31f5a73288c7
jeremiasleite <jeremias23456@gmail.com>
2020-05-15 16:24:53 -0300

    add auth to imoveis endpoints

--- src/imoveis/imoveis.controller.ts
+++ src/imoveis/imoveis.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Delete, UseGuards } from '@nestjs/common';
 import { Imovel } from './interface/imovel.interface';
 import { ImoveisService } from './imoveis.service';
 import { CreateImovelDto } from './dto/create-imovelDto';
 import { ImputImageImovelDto } from './dto/addImage-imovelDto';
 import { UpdateImovelDto } from './dto/update-imovelDto';
-import { IdImovelDto } from './dto/inputuId-imovelDto';
 import { InputSituacaoImovelDto } from './dto/situacao-imovelDto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('imoveis')
 export class ImoveisController {
@@ -23,35 +23,40 @@ export class ImoveisController {
     }
 
     @Post()
+    @UseGuards(JwtAuthGuard)
     async create(@Body() createImovelDto: CreateImovelDto): Promise<Imovel> {
         return this.imoveisService.create(createImovelDto);
     }
 
     @Post(':idImovel/add_imagem')
+    @UseGuards(JwtAuthGuard)
     async addImage(@Param('idImovel') idImovel: string, @Body() image: ImputImageImovelDto) {
         return this.imoveisService.addImage(idImovel, image);
     }
 
     @Put()
+    @UseGuards(JwtAuthGuard)
     async atualizar(@Body() updateImovelDto: UpdateImovelDto): Promise<boolean> {
         return this.imoveisService.update(updateImovelDto);
     }
 
     @Put('atualizar_situacao')
+    @UseGuards(JwtAuthGuard)
     async atualizarSituacao(@Body() situacaoDto: InputSituacaoImovelDto): Promise<boolean> {        
         return this.imoveisService.updateSituacao(situacaoDto.id, situacaoDto.situacao);
     }
 
     @Put(':idImovel/atualizar_imagem/:idImage')
+    @UseGuards(JwtAuthGuard)
     async atualizarImage(
         @Param('idImovel') idImovel: string, 
         @Param('idImage') idImage: string, 
         @Body() image: ImputImageImovelDto) {
         return this.imoveisService.updateImage(idImovel, idImage, image);
     }
-
     
     @Delete(':idImovel/remove_imagem/:idImage')
+    @UseGuards(JwtAuthGuard)
     async removeImage(@Param('idImovel') idImovel: string, @Param('idImage') idImage: string) {
         return this.imoveisService.removeImage(idImovel, idImage);
     }
