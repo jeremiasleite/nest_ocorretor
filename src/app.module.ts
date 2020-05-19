5d0902ac1ff35807f049af5980337419c6b8bb80
jeremiasleite <jeremias23456@gmail.com>
2020-05-18 23:00:21 -0300

    add serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { ImoveisModule } from './imoveis/imoveis.module';
 import configuration from './config/configuration';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(configuration().databaseUrl),    
+    MongooseModule.forRoot(configuration().databaseUrl),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api*'],
+    }),   
     AuthModule,
     UsersModule,
     ImoveisModule],
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT || 3001);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
